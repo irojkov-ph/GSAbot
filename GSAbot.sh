34108d4bd9513286d2635418253be446edd9b9d9
ivan <ivan@ivan-ThinkPad-T61p>
2020-12-06 17:51:36 +0100

    Change version to 0.1.6

--- GSAbot.sh
+++ GSAbot.sh
@@ -19,7 +19,7 @@
 #############################################################################
 
 # GSAbot version
-GSAbot_VERSION='0.1.4'
+GSAbot_VERSION='0.1.6'
 
 # check whether GSAbot.conf is available and source it
 if [ -f $HOME/.GSAbot/GSAbot.conf ]; then
